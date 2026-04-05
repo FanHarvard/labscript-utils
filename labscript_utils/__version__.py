22d6a79d2daa453342c1f8b3a249f67581bc7178
SHU Kenji <kenji0923@gmail.com>
2026-04-05 01:36:41 -0400

    Try metadata version first to avoid console popup
    Happens in GUI app

--- labscript_utils/__version__.py
+++ labscript_utils/__version__.py
@@ -4,24 +4,20 @@ try:
 except ImportError:
     import importlib_metadata
 
-root = Path(__file__).parent.parent
-if (root / '.git').is_dir():
-    try:
-        from setuptools_scm import get_version
-        VERSION_SCHEME = {
-            "version_scheme": "release-branch-semver",
-            "local_scheme": "node-and-date",
-        }
-        scm_version = get_version(root, **VERSION_SCHEME)
-    except ImportError:
-        scm_version = None
-else:
-    scm_version = None
+try:
+    __version__ = importlib_metadata.version(__package__)
+except importlib_metadata.PackageNotFoundError:
+    root = Path(__file__).parent.parent
+    if (root / '.git').is_dir():
+        try:
+            from setuptools_scm import get_version
 
-if scm_version is not None:
-    __version__ = scm_version
-else:
-    try:
-        __version__ = importlib_metadata.version(__package__)
-    except importlib_metadata.PackageNotFoundError:
+            VERSION_SCHEME = {
+                "version_scheme": "release-branch-semver",
+                "local_scheme": "node-and-date",
+            }
+            __version__ = get_version(root, **VERSION_SCHEME)
+        except ImportError:
+            __version__ = None
+    else:
         __version__ = None
